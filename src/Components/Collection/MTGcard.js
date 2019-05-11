1d3bc052e64911046de96ebeeb8345fd09c235ef
Joseph Lorenzo <josephclorenzo@gmail.com>
2019-05-11 15:22:41 -0400

    have data from magic api working

---
+++ src/Components/Collection/MTGcard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MTGcard extends Component {
+    render() {
+        return (
+            <div>
+                "im a card!"
+            </div>
+        );
+    }
+}
+
+export default MTGcard;
--- src/Components/Collection/cardContainer.js
+++ src/Components/Collection/cardContainer.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react'
+import MTGcard from "./MTGcard"
 
-const cardContainer = (props) => {
-  return (
-    <div> "i'm connected!" </div>
-  )
+class cardContainer extends Component {
+
+  state = {
+    formValue: "",
+  }
+
+  componentDidMount() {
+
+  }
+
+  foundCards = (objects) => {
+    const cards = objects.forEach(obj => console.log(obj.name))
+    return cards;
+  }
+
+
+  formHandler = async (event) => {
+    event.preventDefault();
+    let url = `http://localhost:3000/collection/?name=${this.state.formValue}`
+    console.log(url);
+    try {
+      const response = await fetch(url) 
+      const json = await response.json();
+      this.foundCards(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      formValue: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>  "i'm connected!" </p>
+        <p> { this.state.formValue }</p>
+        <form onSubmit={this.formHandler}> 
+
+          <label>Card Name</label>
+          <input 
+            type="text" 
+            // value={this.state.formValue}
+            onChange={this.changeHandler}
+          />
+          <button>Submit</button>
+
+        </form>
+        <MTGcard/>
+    </div>
+    )
+  }
 }
+
 export default cardContainer
